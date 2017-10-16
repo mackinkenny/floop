3cb3c1182c4aa54e53c7f81367d14d6390643d45
mackinkenny <Mackinkenny@gmail.com>
2017-10-17 01:08:26 +0600

    1.2

--- local/resources/views/show/discounts.blade.php
+++ local/resources/views/show/discounts.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
+    <div class="container-spec container">
+        <div class="p-3 row justify-content-center align-items-center">
+            <p class="fs-xs-20 fs-md-30 font-weight-bold">Скидки</p>
+        </div>
+    </div>
+
     @include('index3')
 
 @endsection
