7fdf0bd294e7364e0ffe27365fac11e428755821
uki7991 <tilek.kubanov@gmail.com>
2017-10-22 15:18:56 +0600

    adawd

--- local/resources/views/layouts/app.blade.php
+++ local/resources/views/layouts/app.blade.php
@@ -211,12 +211,13 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="/js/main.js"></script>
+    
     <script src="/js/app.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="/js/main.js"></script>
+
 
     <script src="/js/main2.js"></script>
-    <script src="/js/main3.min.js"></script>
+    <script src="/js/main3.js"></script>
     <script src="/js/main4.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
     <script type="text/javascript">
