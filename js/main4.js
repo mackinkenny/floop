4fe6acc0a6119ad53e419c6942417e1e3d796615
mackinkenny <Mackinkenny@gmail.com>
2017-10-22 17:13:30 +0600

    debug 2

--- js/main4.js
+++ js/main4.js
@@ -34,89 +34,92 @@ $(document).ready(function() {
                     var active = 0;
                     $('#myTab').empty();
                     $('#myTabContent').empty();
-                    for (var i = 0; i < data.center.max_stages; i++) {
-                        for (var boutic of data.boutics) {
-                            if(l === 0 && i + 1 === boutic.stage) {
-                                $('#myTab').append(
-                                    '<li class="nav-item">' +
-                                        '<a class="nav-link active" id="home-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="home" aria-expanded="true">' + (i + 1) + '</a>' +
-                                    '</li>'
-                                );
-                                l = 1;
-                                active = i;
+                    // for (var i = 0; i < data.center.max_stages; i++) {
+                    //     for (var boutic of data.boutics) {
+                    //         if(l === 0 && i + 1 === boutic.stage) {
+                    //             $('#myTab').append(
+                    //                 '<li class="nav-item">' +
+                    //                     '<a class="nav-link active" id="home-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="home" aria-expanded="true">' + (i + 1) + '</a>' +
+                    //                 '</li>'
+                    //             );
+                    //             l = 1;
+                    //             active = i;
+                    //
+                    //             break;
+                    //
+                    //         }
+                    //         else if (i + 1 === boutic.stage && l === 1) {
+                    //             $('#myTab').append(
+                    //                 '<li class="nav-item">' +
+                    //                     '<a class="nav-link" id="profile-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="profile">' + (i + 1) + '</a>' +
+                    //                 '</li>'
+                    //             );
+                    //             break;
+                    //         }
+                    //     }
+                    // }
 
-                                break;
-
-                            }
-                            else if (i + 1 === boutic.stage && l === 1) {
-                                $('#myTab').append(
-                                    '<li class="nav-item">' +
-                                        '<a class="nav-link" id="profile-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="profile">' + (i + 1) + '</a>' +
-                                    '</li>'
-                                );
-                                break;
-                            }
-                        }
-                    }
-
-                    for (var j = 0; j < data.center.max_stages; j++) {
-
-                        if(j == active) {
-                            $('#myTabContent').append(
-                                '<div class="tab-pane fade show active" id="' + j + '-show" role="tabpanel" aria-labelledby="home-tab">' +
-                                    '<div class="row">' +
-                                    '</div>' +
-                                '</div>'
-                            );
-                            for (var boutic of data.boutics) {
-                                console.log(boutic.stage === j + 1);
-                                console.log(boutic.stage);
-
-                                if (boutic.stage == (j + 1)) {
-
-                                        $('#myTabContent .tab-pane.active .row').append(
-                                            '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
-                                            '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
-                                            '<p class="text-center text-dark">' + boutic.name + '</p>' +
-                                            '</a>' +
-                                            '</div>'
-                                        );
-
-                                }
-                                else {
-                                }
-
-                            }
-                        }
-                        else {
-                            $('#myTabContent').append(
-                                '<div class="tab-pane fade non-active" id="' + j + '-show" role="tabpanel" aria-labelledby="profile-tab">' +
-                                    '<div class="row">' +
-                                    '</div>' +
-                                '</div>'
-                            );
-                            for (var boutic of data.boutics) {
-
-                                console.log(boutic.stage == j + 1);
-                                if (boutic.stage == (j + 1)) {
-
-                                        $('#myTabContent .tab-pane.non-active .row').append(
-                                            '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
-                                            '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
-                                            '<p class="text-center text-dark">' + boutic.name + '</p>' +
-                                            '</a>' +
-                                            '</div>'
-                                        );
+                    // for (var j = 0; j < data.center.max_stages; j++) {
+                    //
+                    //     if(j == active) {
+                    //         $('#myTabContent').append(
+                    //             '<div class="tab-pane fade show active" id="' + j + '-show" role="tabpanel" aria-labelledby="home-tab">' +
+                    //                 '<div class="row">' +
+                    //                 '</div>' +
+                    //             '</div>'
+                    //         );
+                    //         for (var boutic of data.boutics) {
+                    //             console.log(boutic.stage === j + 1);
+                    //             console.log(boutic.stage);
+                    //
+                    //             if (boutic.stage == (j + 1)) {
+                    //
+                    //                     $('#myTabContent .tab-pane.active .row').append(
+                    //                         '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
+                    //                         '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
+                    //                         '<p class="text-center text-dark">' + boutic.name + '</p>' +
+                    //                         '</a>' +
+                    //                         '</div>'
+                    //                     );
+                    //
+                    //             }
+                    //             else {
+                    //             }
+                    //
+                    //         }
+                    //     }
+                    //     else {
+                    //         $('#myTabContent').append(
+                    //             '<div class="tab-pane fade non-active" id="' + j + '-show" role="tabpanel" aria-labelledby="profile-tab">' +
+                    //                 '<div class="row">' +
+                    //                 '</div>' +
+                    //             '</div>'
+                    //         );
+                    //         for (var boutic of data.boutics) {
+                    //
+                    //             console.log(boutic.stage == j + 1);
+                    //             if (boutic.stage == (j + 1)) {
+                    //
+                    //                     $('#myTabContent .tab-pane.non-active .row').append(
+                    //                         '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
+                    //                         '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
+                    //                         '<p class="text-center text-dark">' + boutic.name + '</p>' +
+                    //                         '</a>' +
+                    //                         '</div>'
+                    //                     );
+                    //
+                    //
+                    //             }
+                    //             else{
+                    //
+                    //             }
+                    //         }
+                    //     }
+                    // }
 
 
-                                }
-                                else{
 
-                                }
-                            }
-                        }
-                    }
-
+                    $('footer').append('Hello')
 
 
 
@@ -149,75 +152,76 @@ $(document).ready(function() {
                     var active = 0;
                     $('#myTab').empty();
                     $('#myTabContent').empty();
-                    for (var i = 0; i < data.center.max_stages; i++) {
-                        for (var boutic of data.boutics) {
-                            if(l === 0 && i + 1 === boutic.stage) {
-                                $('#myTab').append(
-                                    '<li class="nav-item">' +
-                                    '<a class="nav-link active" id="home-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="home" aria-expanded="true">' + (i + 1) + '</a>' +
-                                    '</li>'
-                                );
-                                l = 1;
-                                active = i;
-                                break;
-
-                            }
-                            else if (i + 1 === boutic.stage && l === 1) {
-                                $('#myTab').append(
-                                    '<li class="nav-item">' +
-                                    '<a class="nav-link" id="profile-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="profile">' + (i + 1) + '</a>' +
-                                    '</li>'
-                                );
-                                break;
-                            }
-                        }
-                    }
-
-                    for (var j = 0; j < data.center.max_stages; j++) {
-                        if(j == active) {
-                            $('#myTabContent').append(
-                                '<div class="tab-pane fade show active" id="' + j + '-show" role="tabpanel" aria-labelledby="home-tab">' +
-                                '<div class="row">' +
-                                '</div>' +
-                                '</div>'
-                            );
-                            for (var boutic of data.boutics) {
-                                if (boutic.stage == j + 1) {
-                                    $('#myTabContent .tab-pane.active .row').append(
-                                        '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
-                                            '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
-                                                '<p class="text-center text-dark">' + boutic.name + '</p>' +
-                                            '</a>' +
-                                        '</div>'
-                                    );
-                                }
-                            }
-                        }
-                        else {
-                            $('#myTabContent').append(
-                                '<div class="tab-pane fade non-active" id="' + j + '-show" role="tabpanel" aria-labelledby="profile-tab">' +
-                                '<div class="row">' +
-                                '</div>' +
-                                '</div>'
-                            );
-                            for (var boutic of data.boutics) {
-                                if (boutic.stage == j + 1) {
-                                    $('#myTabContent .tab-pane.non-active .row').append(
-                                        '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
-                                        '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
-                                        '<p class="text-center text-dark">' + boutic.name + '</p>' +
-                                        '</a>' +
-                                        '</div>'
-                                    );
-                                }
-                            }
-                        }
-                    }
+                    // for (var i = 0; i < data.center.max_stages; i++) {
+                    //     for (var boutic of data.boutics) {
+                    //         if(l === 0 && i + 1 === boutic.stage) {
+                    //             $('#myTab').append(
+                    //                 '<li class="nav-item">' +
+                    //                 '<a class="nav-link active" id="home-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="home" aria-expanded="true">' + (i + 1) + '</a>' +
+                    //                 '</li>'
+                    //             );
+                    //             l = 1;
+                    //             active = i;
+                    //             break;
+                    //
+                    //         }
+                    //         else if (i + 1 === boutic.stage && l === 1) {
+                    //             $('#myTab').append(
+                    //                 '<li class="nav-item">' +
+                    //                 '<a class="nav-link" id="profile-tab" data-toggle="tab" href="#' + i + '-show" role="tab" aria-controls="profile">' + (i + 1) + '</a>' +
+                    //                 '</li>'
+                    //             );
+                    //             break;
+                    //         }
+                    //     }
+                    // }
+                    //
+                    // for (var j = 0; j < data.center.max_stages; j++) {
+                    //     if(j == active) {
+                    //         $('#myTabContent').append(
+                    //             '<div class="tab-pane fade show active" id="' + j + '-show" role="tabpanel" aria-labelledby="home-tab">' +
+                    //             '<div class="row">' +
+                    //             '</div>' +
+                    //             '</div>'
+                    //         );
+                    //         for (var boutic of data.boutics) {
+                    //             if (boutic.stage == j + 1) {
+                    //                 $('#myTabContent .tab-pane.active .row').append(
+                    //                     '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
+                    //                         '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
+                    //                             '<p class="text-center text-dark">' + boutic.name + '</p>' +
+                    //                         '</a>' +
+                    //                     '</div>'
+                    //                 );
+                    //             }
+                    //         }
+                    //     }
+                    //     else {
+                    //         $('#myTabContent').append(
+                    //             '<div class="tab-pane fade non-active" id="' + j + '-show" role="tabpanel" aria-labelledby="profile-tab">' +
+                    //             '<div class="row">' +
+                    //             '</div>' +
+                    //             '</div>'
+                    //         );
+                    //         for (var boutic of data.boutics) {
+                    //             if (boutic.stage == j + 1) {
+                    //                 $('#myTabContent .tab-pane.non-active .row').append(
+                    //                     '<div class="col-4 p-2 col-md-3 my-4 my-md-5">' +
+                    //                     '<a class="boutic-in-center" href="/profile/' + boutic.user_id + '"><img style="width: 100%; height: auto;" class=""  src="/uploads/boutic/avatars/' + boutic.img_path + '" alt="">' +
+                    //                     '<p class="text-center text-dark">' + boutic.name + '</p>' +
+                    //                     '</a>' +
+                    //                     '</div>'
+                    //                 );
+                    //             }
+                    //         }
+                    //     }
+                    // }
 
 
 
                     console.log('Access out');
                     alert('Access out');
+                    $('footer').append('Bye')
                 },
                 error: function () {
                     alert('ERROR');
